6e1e82536db94e94b42a9599486933b37e43ffc5
jaewoo <wodn0510@shift.co.kr>
2022-09-23 17:21:55 +0900

    스프링부트 게시판 만들기 2-05 리포지토리

---
+++ haru/src/main/java/com/mysite/haru/HelloLombok.java
@@ -0,0 +1,20 @@
+package com.mysite.haru;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloLombok {
+
+    private final String hello;
+    private final int lombok;
+
+    public static void main (String[] args) {
+        HelloLombok helloLombok = new HelloLombok("hello",7);
+
+        System.out.println(helloLombok.getHello());
+        System.out.println(helloLombok.getLombok());
+    }
+}
---
+++ haru/src/main/java/com/mysite/haru/QuestionRepository.java
@@ -0,0 +1,6 @@
+package com.mysite.haru;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+}
