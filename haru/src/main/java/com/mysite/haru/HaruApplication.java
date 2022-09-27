c2255ef0c9e1e0a521b2088141f153e1cb1997dd
jaewoo kim <rlawodn0510@yahoo.co.jp>
2022-09-27 09:17:25 +0900

    깃 주소 테스트3

--- haru/src/main/java/com/mysite/haru/HaruApplication.java
+++ haru/src/main/java/com/mysite/haru/HaruApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HaruApplication {
-//git test
+//git test3
 	public static void main(String[] args) {
 		SpringApplication.run(HaruApplication.class, args);
 	}
